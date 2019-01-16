f9eb7f434078519de958645279c80e6bf9da2bcc
doyeon113 <46617177+doyeon113@users.noreply.github.com>
2019-01-16 13:39:35 +0900

    픽셀충돌중

--- playerManager.cpp
+++ playerManager.cpp
@@ -13,12 +13,25 @@ playerManager::~playerManager()
 
 HRESULT playerManager::init()
 {
-	_rc[BALEOG] = RectMake(30, 30, 150, 150);
-	_rc[ERIC] = RectMake(130, 30, 157, 157);
-	_rc[OLAF] = RectMake(250, 30, 160, 160);
+	for (int i = 0; i < 3; ++i)
+	{
+		_x[i] = 30 + 120 * i;
+		_y[i] = 65;
+	}
 
-	_isDebug = false;
+	for (int i = 0; i < 3; ++i)
+	{
+		_rc[i] = RectMake(_x[i], _y[i], 100, 100);
+	}
 
+	_isDebug = true;
+
+	for (int i = 0; i < 3; ++i)
+	{
+		//센터
+		_prove_X[i] = (_rc[i].right - _rc[i].left)/2 + _rc[i].left;
+		_prove_Y[i] = _rc[i].bottom;
+	}
 
 	return S_OK;
 }
@@ -29,6 +42,7 @@ void playerManager::release()
 
 void playerManager::update()
 {
+	//캐릭터박스 보이기용
 	if (KEYMANAGER->isOnceKeyDown(VK_NUMPAD1))
 	{
 		if (_isDebug)
@@ -41,7 +55,34 @@ void playerManager::update()
 		}
 	}
 
+	pixelCollision();
 
+
+	if (KEYMANAGER->isStayKeyDown('A'))
+	{
+		_x[0] -= 3;
+	}
+	if (KEYMANAGER->isStayKeyDown('D'))
+	{
+		_x[0] += 3;
+	}
+	if (KEYMANAGER->isStayKeyDown('S'))
+	{
+		_y[0] += 3;
+	}
+	if (KEYMANAGER->isStayKeyDown('W'))
+	{
+		_y[0] -= 3;
+	}
+
+
+	//갱신
+	for (int i = 0; i < 3; ++i)
+	{
+		_rc[i] = RectMake(_x[i], _y[i], 100, 100);
+		_prove_X[i] = _x[i] + 50;
+		_prove_Y[i] = _y[i] + 100;
+	}
 }
 
 void playerManager::render()
@@ -50,7 +91,30 @@ void playerManager::render()
 	{
 		for (int i = 0; i < 3; ++i)
 		{
-			Rectangle(getMemDC(), _rc[i]);
+			Rectangle(getMemDC(), _rc[0]);
+		}
+	}
+
+	char str[100];
+	sprintf_s(str, "%d", _prove_Y[BALEOG]);
+	TextOut(getMemDC(), 300, 100, str, strlen(str));
+}
+
+void playerManager::pixelCollision()
+{
+	for (int i = _prove_Y[BALEOG] - 50; i < _prove_Y[BALEOG] + 50; ++i)
+	{
+		COLORREF color = GetPixel(IMAGEMANAGER->findImage("pixel")->getMemDC(), _x[BALEOG], i);
+
+		int r = GetRValue(color);
+		int g = GetGValue(color);
+		int b = GetBValue(color);
+
+		if (r == 0 && g == 255 && b == 0)
+		{
+			_y[i] = i - 100;
+
+			break;
 		}
 	}
 }
--- playerManager.h
+++ playerManager.h
@@ -16,6 +16,10 @@ private:
 	bool _isDebug;
 
 
+	float _x[3];
+	float _y[3];
+	int _prove_X[3];
+	int _prove_Y[3];
 
 
 public:
@@ -26,5 +30,7 @@ public:
 	void release();
 	void update();
 	void render();
+
+	void pixelCollision();
 };
 
