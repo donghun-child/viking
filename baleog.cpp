67dde1c1f2666a92d0836ecc5e75fefce4cebef9
donghun <bbang0751@naver.com>
2019-01-16 11:51:40 +0900

    벨로그추가

--- baleog.cpp
+++ baleog.cpp
@@ -10,3 +10,32 @@ baleog::baleog()
 baleog::~baleog()
 {
 }
+
+HRESULT baleog::init()
+{
+	_baleogPlayer.baleogImage = IMAGEMANAGER->addFrameImage("벨로그", "image/Baleog.bmp", 1200, 1950, 8, 13, true, RGB(255, 0, 255));
+	_baleogPlayer.x = BALEOG_X;
+	_baleogPlayer.y = BALEOG_Y;
+	_baleogPlayer.speed = BALEOG_SPEED;
+	_baleogPlayer.baleogRc = RectMakeCenter(_baleogPlayer.x, _baleogPlayer.y, _baleogPlayer.baleogImage->getFrameWidth(), _baleogPlayer.baleogImage->getFrameHeight());
+
+	_baleogState = BALEOG_RIGHT_STOP; //처음엔 오른쪽 기본상태로
+
+	int rightStop[] = { 0,1 };
+	//KEYANIMANAGER->addArrayFrameAnimation("")
+
+
+	return S_OK;
+}
+
+void baleog::release()
+{
+}
+
+void baleog::update()
+{
+}
+
+void baleog::render()
+{
+}
--- baleog.h
+++ baleog.h
@@ -1,8 +1,51 @@
 #pragma once
-class baleog
+#include "gameNode.h"
+
+struct baleogPlayer
 {
+	float x, y;
+	image* baleogImage;
+	RECT baleogRc;
+	float speed;
+};
+
+
+enum BALEOGSTATE //벨로그 상태
+{
+	BALEOG_RIGHT_STOP, //오른쪽 기본상태
+	BALEOG_LEFT_STOP, //왼쪽 기본상태
+	BALEOG_RIGHT_MOVE, //오른쪽 움직임
+	BALEOG_LEFT_MOVE, //왼쪽 움직임
+	BALEOG_RIGHT_SWORD_ATTACK, //오른쪽 검공격
+	BALEOG_LEFT_SWORD_ATTACK, //왼쪽 검공격
+	BALEOG_RIGHT_ARROW_ATTACK, //오른쪽 화살공격
+	BALEOG_LEFT_ARROW_ATTACK, //왼쪽 화살공격
+};
+
+enum BALEOGINFO // 벨로그 정보들
+{
+	BALEOG_X = WINSIZEX / 2,
+	BALEOG_Y = WINSIZEY / 2,
+	BALEOG_SPEED = 0,
+};
+
+class baleog : public gameNode
+{
+private:
+	BALEOGSTATE _baleogState;
+	baleogPlayer _baleogPlayer;
+
+	animation* _baleogMotion; //벨로그 모션 담아줄 빈 애니메이션
 public:
+
 	baleog();
 	~baleog();
+
+	HRESULT init();
+	void release();
+	void update();
+	void render();
+
+
 };
 
