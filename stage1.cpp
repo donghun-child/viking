b8fae4295707b47d36b13490c7223f86f3e39ef7
doyeon113 <46617177+doyeon113@users.noreply.github.com>
2019-01-16 18:39:22 +0900

    픽셀 맵 수정

--- stage1.cpp
+++ stage1.cpp
@@ -14,7 +14,7 @@ stage1::~stage1()
 HRESULT stage1::init()
 {
 	IMAGEMANAGER->addImage("backGround", "image/map.bmp", 3000, 2240, true, RGB(255, 0, 255));
-	IMAGEMANAGER->addImage("pixel", "image/pixel.bmp", 3100, 2240, true, RGB(255, 0, 255));
+	IMAGEMANAGER->addImage("pixel", "image/pixel.bmp", 3000, 2240, true, RGB(255, 0, 255));
 
 	_camera = new camera;
 	_camera->init(0, 0 , 3);
@@ -59,7 +59,7 @@ void stage1::render()
 	IMAGEMANAGER->render("backGround", getMemDC(), 0, 0, _camera->getCameraX(), _camera->getCameraY(), WINSIZEX, WINSIZEY);
 	if (_isPixel)
 	{
-		IMAGEMANAGER->render("pixel", getMemDC(), -50, 0, _camera->getCameraX(), _camera->getCameraY(), WINSIZEX + 50, WINSIZEY);
+		IMAGEMANAGER->render("pixel", getMemDC(), 0, 0, _camera->getCameraX(), _camera->getCameraY(), WINSIZEX , WINSIZEY);
 	}
 	_playerManager->render();
 }
