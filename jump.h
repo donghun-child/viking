511679cbd8ac16622000e3ea90e274ecae903feb
donghun <bbang0751@naver.com>
2019-01-17 11:22:32 +0900

    다시수정 죄송합니다

--- jump.h
+++ jump.h
@@ -4,32 +4,11 @@
 class jump : public gameNode
 {
 private:
-	float _jumpPower;
-	float _gravity;
 
-	float* _x;
-	float* _y;
-
-	float _startX, _startY;
-
-	bool _isJumping;
 
 public:
 	jump();
 	~jump();
 
-	HRESULT init();
-	void release();
-	void update();
-	void render();
-
-	void jumping(float* x, float* y, float jumpPower, float gravity);
-};
-
-class jump
-{
-public:
-	jump();
-	~jump();
 };
 
