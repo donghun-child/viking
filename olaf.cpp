535097a80b31a640097c2c4b5d0c1f6a1546acc5
woo921127 <shany21@naver.com>
2019-01-18 17:30:11 +0900

    olaf
    이닛부분 갱신

--- olaf.cpp
+++ olaf.cpp
@@ -13,10 +13,50 @@ olaf::~olaf()
 
 HRESULT olaf::init()
 {
+	_olafimg = IMAGEMANAGER->addFrameImage("olaf", "image\\olaf.bmp", 0, 0, 1280, 2560, 8, 16, true, RGB(255, 0, 255));
 
-	_olaf_x = WINSIZEX / 2;
-	_olaf_y = WINSIZEY / 2;
+	_olafDirection = OLAF_DIRECTION_RIGHT_STOP;
 
+	_olaf_x = 100;
+	_olaf_y = 100;
+
+	_olaf_rc = RectMakeCenter(_olaf_x, _olaf_y, _olafimg->getFrameWidth(), _olafimg->getFrameHeight());
+
+
+	int rightStop[] = { 0 };
+	KEYANIMANAGER->addArrayFrameAnimation(_olafName, "olafRightStop", "olaf", rightStop, 1, 6, true);
+	int rightShieldup[] = { 1 };
+	KEYANIMANAGER->addArrayFrameAnimation(_olafName, "olafRightShield", "olaf", rightShieldup, 1, 6, true);
+	int leftStop[] = { 2 };
+	KEYANIMANAGER->addArrayFrameAnimation(_olafName, "olafLeftStop", "olaf", leftStop, 1, 6, true);
+	int leftShieldup[] = { 3 };
+	KEYANIMANAGER->addArrayFrameAnimation(_olafName, "olafRightShield", "olaf", leftShieldup, 1, 6, true);
+	int rightMove[] = { 8, 9,10,11,12,13,14,15 };
+	KEYANIMANAGER->addArrayFrameAnimation(_olafName, "olafRightMove", "olaf", rightMove, 8, 6, true);
+	int leftMove[] = { 23,22,21,20,19,18,17,16 };
+	KEYANIMANAGER->addArrayFrameAnimation(_olafName, "olafLeftMove", "olaf", leftMove, 8, 6, true);
+	int rightShieldUpMove[] = { 24,25,26,27,28,29,30,31 };
+	KEYANIMANAGER->addArrayFrameAnimation(_olafName, "olafRightShieldUpMove", "olaf", rightShieldUpMove, 8, 6, true);
+	int leftShieldUpMove[] = { 39,38,37,36,35,34,33,32 };
+	KEYANIMANAGER->addArrayFrameAnimation(_olafName, "olafLeftShieldUpMove", "olaf", leftShieldUpMove, 8, 6, true);
+	int SadariUp[] = { 33,34,35,36 };
+	KEYANIMANAGER->addArrayFrameAnimation(_olafName, "olafSadariUp", "olaf", SadariUp, 4, 6, true);
+
+
+	int rightPush[] = { 64,65,66,67 };
+	KEYANIMANAGER->addArrayFrameAnimation(_olafName, "olafRightPush", "olaf", rightPush, 4, 6, true);
+	int leftPush[] = { 68,69,70,71 };
+	KEYANIMANAGER->addArrayFrameAnimation(_olafName, "olafLeftPush", "olaf", leftPush, 4, 6, true);
+
+	_olafMotion = KEYANIMANAGER->findAnimation(_olafName, "olafRightStop");
+
+	_Wax = WINSIZEX / 2;
+	_Way = 150;
+
+	_wall = RectMakeCenter(_Wax, _Way, 30, 100);
+
+	num = 0;
+	_changeMode = false;
 
 	return S_OK;
 }
@@ -59,9 +99,11 @@ void olaf::update(POINTFLOAT StagePos, int choice)
 			}
 		}
 	}
+	olafMovement();
 
+	_olaf_rc = RectMakeCenter(_olaf_x, _olaf_y, _olafimg->getFrameWidth(), _olafimg->getFrameHeight());
 
-	//	KEYANIMANAGER->update();
+	KEYANIMANAGER->update();
 }
 
 void olaf::render()
