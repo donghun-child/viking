7a138795ea8280c136954b0c44e264d983786aa8
doyeon113 <46617177+doyeon113@users.noreply.github.com>
2019-01-20 18:34:05 +0900

    아이템 생성

--- item.cpp
+++ item.cpp
@@ -10,3 +10,82 @@ item::item()
 item::~item()
 {
 }
+
+HRESULT item::init(const char* imageName, bool isFrame, int frameY)
+{
+	_isFrame = isFrame;
+	_itemName = imageName;
+	_frameY = frameY;
+
+	return S_OK;
+}
+
+void item::release()
+{
+	for (_viItem = _vItem.begin(); _viItem != _vItem.end(); ++_viItem)
+	{
+		_viItem = _vItem.erase(_viItem);
+
+		--_viItem;
+	}
+}
+
+void item::update()
+{
+	removeItem();
+
+
+
+}
+
+void item::render()
+{
+	for (_viItem = _vItem.begin(); _viItem != _vItem.end(); ++_viItem)
+	{
+		if (!_isFrame)
+		{
+			_viItem->image->render(getMemDC(), _viItem->x, _viItem->y);
+		}
+		else
+		{
+			_viItem->image->frameRender(getMemDC(), _viItem->x, _viItem->y,_viItem->frameX, _frameY);
+		}
+	}
+}
+
+void item::createItem(float x, float y, float width, float height)
+{
+	tagItem item;
+	ZeroMemory(&item, sizeof(item));
+	item.image = IMAGEMANAGER->findImage(_itemName);
+	item.frameX = 0;
+	item.frameY = _frameY;
+	item.x = x;
+	item.y = y;
+	item.isLive = true;
+	if (!_isFrame)
+	{
+		item.rc = RectMake(x, y, item.image->GetWidth(), item.image->GetHeight());
+	}
+	else
+	{
+		item.rc = RectMake(x, y, item.image->getFrameWidth(), item.image->getFrameHeight());
+	}
+
+	_vItem.push_back(item);
+}
+
+void item::removeItem()
+{
+	for (_viItem = _vItem.begin(); _viItem != _vItem.end(); )
+	{
+		if ((*_viItem).isLive == false)
+		{
+			_vItem.erase(_viItem);
+		}
+		else
+		{
+			++_viItem;
+		}
+	}
+}
--- item.h
+++ item.h
@@ -1,8 +1,43 @@
 #pragma once
-class item
+#include "gameNode.h"
+#include <vector>
+
+struct tagItem
 {
+	image* image;
+	float x;
+	float y;
+	RECT rc;
+	bool isLive;
+	int frameX;
+	int frameY;
+};
+
+class item : public gameNode
+{
+private:
+	vector<tagItem>				_vItem;
+	vector<tagItem>::iterator	_viItem;
+
+	bool _isFrame;
+	const char* _itemName;
+	int _frameY;
+
 public:
 	item();
 	~item();
+
+	HRESULT init(const char* imageName,bool isFrame = 0,int frameY = 0);
+	void release();
+	void update();
+	void render();
+
+	void createItem(float x, float y,float width,float height);
+	void removeItem();
+
+	vector<tagItem> getVItem() { return _vItem; }
+	vector<tagItem>* getVItemAddress() { return &_vItem; }
+	vector<tagItem>::iterator getViItem() { return _viItem; }
+
 };
 
