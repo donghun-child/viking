3ac4067ceb5a8f042316233ffe55b853fad5d6b4
vebitas <46617166+vebitas@users.noreply.github.com>
2019-01-21 14:05:49 +0900

    life image size change

--- ui.cpp
+++ ui.cpp
@@ -16,6 +16,8 @@ HRESULT ui::init()
 {
 	IMAGEMANAGER->addImage("UI", "image/ui.bmp", 1000, 191, true, RGB(255, 0, 255));
 	IMAGEMANAGER->addFrameImage("profile", "image/profile.bmp", 375, 270, 3, 3, false, RGB(0, 0, 0));
+	IMAGEMANAGER->addImage("life", "image/life.bmp", 25, 25, true, RGB(255, 0, 255));
+
 
 	_ericCurrentX = _ericCurrentY = 0;
 	_baleogCurrentX = 1;
@@ -40,11 +42,12 @@ void ui::render()
 	IMAGEMANAGER->findImage("profile")->frameRender(getMemDC(), 62, 671, _ericCurrentX, _ericCurrentY);
 	IMAGEMANAGER->findImage("profile")->frameRender(getMemDC(), 344, 671, _baleogCurrentX, _baleogCurrentY);
 	IMAGEMANAGER->findImage("profile")->frameRender(getMemDC(), 625, 671, _olafCurrentX, _olafCurrentY);
+	IMAGEMANAGER->findImage("life")->render(getMemDC(), 100, 600);
 
 
-	char str[128];
-	sprintf_s(str, "초이스 : %d", _choice);
-	TextOut(getMemDC(), 100, 100, str, strlen(str));
+	//char str[128];
+	//sprintf_s(str, "초이스 : %d", _choice);
+	//TextOut(getMemDC(), 100, 100, str, strlen(str));
 }
 
 void ui::profileUpdate(int choice)
